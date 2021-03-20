9c7a21e96147be9ebdde4ca1e879421e4f5c07d8
Matheus Silva <msilvadev@Matheuss-MacBook-Pro.local>
2021-03-20 16:23:29 -0300

    allow cors

--- src/main/java/br/com/sigowebapi/cors/CorsFilter.java
+++ src/main/java/br/com/sigowebapi/cors/CorsFilter.java
@@ -24,10 +24,10 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
 
-        response.setHeader("Access-Control-Allow-Origin", webApiProperty.getOriginAllowed());
+        //response.setHeader("Access-Control-Allow-Origin", webApiProperty.getOriginAllowed());
         response.setHeader("Access-Control-Allow-Credentials", "true");
 
-        if ("OPTIONS".equals(request.getMethod()) && webApiProperty.getOriginAllowed().equals(request.getHeader("Origin"))) {
+        if ("OPTIONS".equals(request.getMethod())) {
             response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
             response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
             response.setHeader("Access-Control-Max-Age", "3600");
